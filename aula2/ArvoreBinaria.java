c1bf32d9095ea97e72424332e10f7ba1b0530db8
DANIEL ROBSON ALEXANDRE SILVA <daniel.silva067@cs.cruzeirodosul.edu.br>
2026-08-12 21:24:35 -0300

    Ultima parte

--- aula2/ArvoreBinaria.java
+++ aula2/ArvoreBinaria.java
@@ -1,7 +1,6 @@
 package aula2;
 
 public class ArvoreBinaria{
-
     static class No{
         String nome;
         No esquerda, direita;
@@ -12,9 +11,7 @@ public class ArvoreBinaria{
         }
     }
 
-
     No raiz; // fica nulo de vez
-
     void inserir(String nome){
         raiz = inserirRecursivo(raiz, nome);
     } // o raiz que é nulo vai retornar um valor recursivo
@@ -23,29 +20,30 @@ public class ArvoreBinaria{
         if(atual == null){
             return new No(nome);
         }
-        return null;
-    }// pegaa a raiz e vai inserindo recursivamente, se for nulo ele cria um novo nó
-
+        if (nome.compareToIgnoreCase(atual.nome) < 0) {
+            atual.esquerda = inserirRecursivo(atual.esquerda, nome);
+        } else if (nome.compareToIgnoreCase(atual.nome) > 0) {
+            atual.direita = inserirRecursivo(atual.direita, nome);
+        }
+        return atual;
+    }
 
     public static void main(String[] args){
         ArvoreBinaria arvore = new ArvoreBinaria();
-
         arvore.inserir("Lucas");
+        arvore.inserir("Amanda");
         System.out.println("Nome inserido: " + arvore.raiz.nome);
+        System.out.println("Nome inserido: " + arvore.raiz.esquerda.nome);
 
 
         //Lista com 10 nomes
         String[] nomes = {
             "Lucas", "Amanda", "Bruno", "Carla", "Eduardo", "Fernanda", "Gustavo", "Helena", "Igor", "Beatriz"
         };
-
         for (String nome: nomes) {
             arvore.inserir(nome);
         }
-
         //Exibir os nomes em ordem
         // arvore.emOrdem();
-
-
     }
     }
